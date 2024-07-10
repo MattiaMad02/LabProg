407aaa1f94bbe536d2160fe79c26934e24e2306d
MattiaMad02 <mattia.madera@edu.unifi.it>
2024-07-10 17:06:54 +0200

    first commit

---
+++ Activity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eugen on 10/07/2024.
+//
+
+#include "Activity.h"
---
+++ Activity.h
@@ -0,0 +1,15 @@
+//
+// Created by eugen on 10/07/2024.
+//
+
+#ifndef LABPROGR_ACTIVITY_H
+#define LABPROGR_ACTIVITY_H
+
+
+class Activity {
+
+
+};
+
+
+#endif //LABPROGR_ACTIVITY_H
