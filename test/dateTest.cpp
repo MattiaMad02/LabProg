fa5672cabe7ac477dd027898e9528ca0325eac1e
MattiaMad02 <mattia.madera@edu.unifi.it>
2024-09-13 16:38:48 +0200

    twelfth commit:modifica dateTest

--- test/dateTest.cpp
+++ test/dateTest.cpp
@@ -4,45 +4,71 @@
 #include "gtest/gtest.h"
 #include "../Date.h"
 #include <fstream>
-
-TEST(DateTest, ValidDate) {
-    Date date(15, 9, 2023);
-    EXPECT_EQ(date.getDay(), 15);
-    EXPECT_EQ(date.getMonth(), 9);
-    EXPECT_EQ(date.getYear(), 2023);
+TEST(DateTest, ConstructorValid) {
+    Date d(15, 8, 2023);
+    EXPECT_EQ(d.getDay(), 15);
+    EXPECT_EQ(d.getMonth(), 8);
+    EXPECT_EQ(d.getYear(), 2023);
 }
 
-TEST(DateTest, InvalidDate) {
-    EXPECT_THROW(Date date(32, 1, 2023), Date::DateException);  // Giorno non valido
-    EXPECT_THROW(Date date(15, 13, 2023), Date::DateException); // Mese non valido
-    EXPECT_THROW(Date date(29, 2, 2021), Date::DateException);  // Non bisestile
+TEST(DateTest, ConstructorInvalid) {
+    EXPECT_THROW(Date d(32, 1, 2023), Date::DateException);
+    EXPECT_THROW(Date d(15, 13, 2023), Date::DateException);
+}
+TEST(DateTest, GetMethods) {
+    Date d(15, 8, 2023);
+    EXPECT_EQ(d.getDay(), 15);
+    EXPECT_EQ(d.getMonth(), 8);
+    EXPECT_EQ(d.getYear(), 2023);
 }
 
-TEST(DateTest, LeapYear) {
-    Date leapDate(29, 2, 2020);
-    EXPECT_EQ(leapDate.getDay(), 29);
-    EXPECT_EQ(leapDate.getMonth(), 2);
-    EXPECT_EQ(leapDate.getYear(), 2020);
 
-    EXPECT_THROW(Date invalidLeapDate(29, 2, 2019),Date::DateException);
+TEST(DateTest, SetMethodsValid) {
+    Date d(15, 8, 2023);
+    d.setDay(20);
+    EXPECT_EQ(d.getDay(), 20);
+    d.setMonth(12);
+    EXPECT_EQ(d.getMonth(), 12);
+    d.setYear(2024);
+    EXPECT_EQ(d.getYear(), 2024);
+}
+TEST(DateTest, SetMethodsInvalid) {
+    Date d(15, 8, 2023);
+    EXPECT_THROW(d.setDay(32), Date::DateException);
+    EXPECT_THROW(d.setMonth(13), Date::DateException);
+    EXPECT_THROW(d.setYear(-1), Date::DateException);
 }
 
+
+TEST(DateTest, IsLeapYear) {
+    Date d1(15, 8, 2020);
+    EXPECT_TRUE(d1.isLeapYear());
+    Date d2(15, 8, 2021);
+    EXPECT_FALSE(d2.isLeapYear());
+}
 TEST(DateTest, DaysInMonth) {
-    Date date(1, 2, 2023);
-    EXPECT_EQ(date.daysInMonth(), 28);
-
-    Date leapYearDate(1, 2, 2020);
-    EXPECT_EQ(leapYearDate.daysInMonth(), 29);
-
-    Date marchDate(1, 3, 2023);
-    EXPECT_EQ(marchDate.daysInMonth(), 31);
+    Date d1(15, 2, 2020); // Leap year
+    EXPECT_EQ(d1.daysInMonth(), 29);
+    Date d2(15, 2, 2021); // Non-leap year
+    EXPECT_EQ(d2.daysInMonth(), 28);
+    Date d3(15, 4, 2021);
+    EXPECT_EQ(d3.daysInMonth(), 30);
+    Date d4(15, 1, 2021);
+    EXPECT_EQ(d4.daysInMonth(), 31);
 }
+TEST(DateTest, PrintMethod) {
+    Date d(15, 8, 2023);
+    testing::internal::CaptureStdout();
+    d.print();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "15/08/2023\n");
+}
+
 
 TEST(DateTest, EqualityOperator) {
-    Date date1(15, 9, 2023);
-    Date date2(15, 9, 2023);
-    Date date3(16, 9, 2023);
-
-    EXPECT_TRUE(date1 == date2);
-    EXPECT_FALSE(date1 == date3);
+    Date d1(15, 8, 2023);
+    Date d2(15, 8, 2023);
+    Date d3(16, 8, 2023);
+    EXPECT_TRUE(d1 == d2);
+    EXPECT_FALSE(d1 == d3);
 }
